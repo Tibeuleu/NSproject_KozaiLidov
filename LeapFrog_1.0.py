0c8bb870327101a0dca9a0b0cae8d38049f7f4bb
Alex_Hubert <alex.hubert@etu.unistra.fr>
2021-10-15 16:24:47 +0200

    initialisation

---
+++ LeapFrog_1.0.py
@@ -0,0 +1,18 @@
+from math import *
+import numpy as np
+
+def main():
+
+    #initialisation
+    x1 = np.array([0, 0, 0])
+    x2 = np.array([1, 0, 0])
+    x3 = np.array([1, 0, 0])
+
+    v1 = np.array([0, 0, 0])
+    v2 = np.array([1, 0, 0])
+    v3 = np.array([1, 0, 0])
+
+
+
+if __name__ == '__main__':
+    main()
